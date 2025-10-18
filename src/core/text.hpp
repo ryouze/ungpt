8ca080385c551df1651272c2dfc2a04fa3b57ec8
ryouze <98982999+ryouze@users.noreply.github.com>
2025-10-18 18:20:41 +0200

    Add docs for functions.

--- src/core/text.hpp
+++ src/core/text.hpp
@@ -8,12 +8,32 @@
 
 #include <cstddef>  // for std::size_t
 #include <string>   // for std::string
+
 namespace core::text {
 
+/**
+ * @brief Remove unwanted characters from the provided text in place.
+ *
+ * @param text String to modify in place (e.g., "hello world").
+ */
 void remove_unwanted_characters(std::string &text);
 
+/**
+ * @brief Count the number of words in the provided text.
+ *
+ * @param text String to analyze (e.g., "hello world")
+ *
+ * @return Number of words in the text (e.g., "2").
+ */
 [[nodiscard]] std::size_t count_words(const std::string &text);
 
+/**
+ * @brief Count the number of characters in the provided text.
+ *
+ * @param text String to analyze (e.g., "hello world").
+ *
+ * @return Number of characters in the text (e.g., "11").
+ */
 [[nodiscard]] std::size_t count_characters(const std::string &text);
 
 }  // namespace core::text
