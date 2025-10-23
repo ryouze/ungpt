e57d1881247ad6e9d3cc44a503170e3c4f558252
ryouze <98982999+ryouze@users.noreply.github.com>
2025-10-23 13:46:33 +0200

    Add logging to to the clipboard module.

--- src/core/clipboard.cpp
+++ src/core/clipboard.cpp
@@ -6,6 +6,7 @@
 
 #include <SFML/System/String.hpp>
 #include <SFML/Window/Clipboard.hpp>
+#include <spdlog/spdlog.h>
 
 #include "core/clipboard.hpp"
 
@@ -16,7 +17,11 @@ std::string read_from_clipboard()
     // sf::Clipboard.getString() returns a sf::String (UTF-32)
     // We convert it to UTF-8 and then to raw std::string
     const sf::U8String utf8 = sf::Clipboard::getString().toUtf8();
-    return {utf8.cbegin(), utf8.cend()};
+    const std::string result = {utf8.cbegin(), utf8.cend()};
+
+    SPDLOG_DEBUG("Read '{}' characters from clipboard: '{}'", result.size(), result);
+
+    return result;
 }
 
 void write_to_clipboard(const std::string &text)
@@ -24,6 +29,8 @@ void write_to_clipboard(const std::string &text)
     // sf::String handles conversion from UTF-8 to UTF-32
     const sf::String utf32 = sf::String::fromUtf8(text.cbegin(), text.cend());
     sf::Clipboard::setString(utf32);
+
+    SPDLOG_DEBUG("Wrote '{}' characters to clipboard: '{}'", text.size(), text);
 }
 
 }  // namespace core::clipboard
